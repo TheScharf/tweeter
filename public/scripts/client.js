3045a58e2c5073bc31c2c05aba8fef8ee51a6986
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-03-01 19:40:37 -0700

    added details to character counter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,22 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  const error = $("#error");
+  error.hide();
+  const newTweetForm = $("#new-tweet-form");
+  
+  newTweetForm.on("submit", function(event) {
+    event.preventDefault();
+    console.log("inside tweet form");
+    const MAX_TWEET_LENGTH = 140;
+    const textLength = $("#tweet-text").val().length;
+    if (textLength > MAX_TWEET_LENGTH) {
+      console.log("max", MAX_TWEET_LENGTH);
+      error.show();
+    }
+  })
+  //console.log(newTweetForm);
+  //console.log(error);
+});
 
