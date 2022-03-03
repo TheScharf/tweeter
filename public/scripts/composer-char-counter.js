e1e661b8e58221db20b2e105c9b054f80daea5b8
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-03-02 17:21:09 -0700

    added tweet placeholder and fixed some minor bugs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,24 +1,13 @@
 $(document).ready(function() {
-  // --- our code goes here ---
-  //console.log('Document is ready!');
+  console.log('Document is ready!');
   $("#tweet-text").on("input", function() {
     let textLength = $(this).val().length;
     const counter = $(".counter");
-    //console.log("textLength", textLength);
     counter.text(140 - textLength);
     if (textLength > 140) {
-      //counter.css("color", "red");
       counter.addClass("negative-counter");
-      
-      //counter.removeClass(".black").addClass(".red").slideDown(400);
-      //$("#error").removeClass("error").addClass("error-show");
     } else {
       counter.removeClass("negative-counter");
-      // $(".counter").removeClass(".red").addClass(".black");
-      // $("#error").removeClass("error-show");
-      // $("#error").addClass("error");
     }
-    //console.log("made it to the end of your code");
-  });
-  
+  });  
 });
